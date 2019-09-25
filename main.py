dfabd200a512b48c5de516fa8626db9c471190c5
Oleg Orlov <43028500+O3DB@users.noreply.github.com>
2019-09-25 19:26:00 +0300

    Remove while true loop from main()

--- main.py
+++ main.py
@@ -14,30 +14,27 @@ logger = logging.getLogger(__name__)
 
 def main():
     setup_logging()
-    while True:
-        logger.info('Start app')
-        try:
-            comic_info, filename = download_random_comic()
-            logger.info(f'Downloaded comic: {filename}')
+    logger.info('Start app')
+    try:
+        comic_info, filename = download_random_comic()
+        logger.info(f'Downloaded comic: {filename}')
 
-            upload_image_to_group_wall(
-                access_token=CONFIG['VK_TOKEN'],
-                group_id=CONFIG['GROUP_ID'],
-                image_path=filename,
-                image_comment=comic_info['alt']
-                )
-            logger.info('Comic uploaded to the group wall')
+        upload_image_to_group_wall(
+            access_token=CONFIG['VK_TOKEN'],
+            group_id=CONFIG['GROUP_ID'],
+            image_path=filename,
+            image_comment=comic_info['alt']
+            )
+        logger.info('Comic uploaded to the group wall')
 
-        except HTTPError:
-            logger.warning('HTTP Error!')
+    except HTTPError:
+        logger.warning('HTTP Error!')
 
-        try:
-            logger.info('Image removed')
-            os.remove(filename)
-        except:
-            continue
-
-        time.sleep(24 * 3600) #1 day sleep
+    try:
+        os.remove(filename)
+        logger.info('Image removed')
+    except:
+        pass
 
 
 if __name__ == '__main__':
