68aed7665213da7fb04ef1ef3a3da8c572be8f60
Oleg Orlov <43028500+O3DB@users.noreply.github.com>
2019-09-25 18:55:46 +0300

    Remove extra comments

--- tools.py
+++ tools.py
@@ -7,13 +7,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-# def fetch_image(url, verify=True):
-#     response = requests.get(url, verify=verify)
-#     response.raise_for_status()
-#     extension = check_url_is_image(response.url)
-#     return response.content, extension
-
-
 def check_url_is_image(url, extensions=['jpg', 'jpeg', 'png', 'bmp', 'tif', 'svg']):
     extension = url.split('.')[-1]
     if extension not in extensions:
@@ -44,9 +37,3 @@ def scan_for_files_in_folder(foldername, recursion=False):
         for filename in filenames:
             files.append(os.path.join(dirpath, filename))
     return files
-
-
-
-
-if __name__ == '__main__':
-    download_image('https://imgs.xkcd.com/comics/earth_temperature_timeline.png')
